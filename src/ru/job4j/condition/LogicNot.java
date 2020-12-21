484fd33608650cb1a9c9649d2520450ad5429a62
alekseeevtest1 <alekseeevtest@yandex.ru>
2020-12-21 12:47:02 +0300

    4.3.3. Логическое отрицание !

---
+++ src/ru/job4j/condition/LogicNot.java
@@ -0,0 +1,68 @@
+package ru.job4j.condition;
+
+import java.util.Scanner;
+
+/**
+ * В нем уже есть методы isEven() - проверяет, что число четное,
+ *
+ * isPositive() -  проверяет, что число положительное.
+ *
+ * Вам нужно написать код в эти методы используя сочетания методов указанных ниже:
+ *
+ * - метод notEven(), проверяет, что число нечетное
+ *
+ * - метод notPositive(), проверяет, что число не положительное
+ *
+ * - метод notEvenAndPositive(), проверяет, что число нечетное и положительное
+ *
+ * - метод evenOrNotPositive(), проверяет, что число четное или не положительное
+ */
+
+public  class LogicNot {
+
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+    public static boolean isPositive(int num) {
+        return num > 0;
+    }
+
+    public static boolean notEven(int num) {
+        return !isEven(num);
+    }
+
+    public static boolean notPositive(int num) {
+        return  !isPositive(num);
+    }
+
+    public static boolean notEvenAndPositive(int num) {
+        if (!isEven(num) && isPositive(num)){
+
+            return true;
+        }
+        return false;
+
+    }
+
+    public static boolean evenOrNotPositive(int num) {
+        if (isEven( num) || notPositive(num)){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        System.out.println(isEven(num));
+        System.out.println(isPositive(num));
+        System.out.println(notEven(num));
+        System.out.println(notPositive(num));
+        System.out.println(notEvenAndPositive(num));
+        System.out.println(evenOrNotPositive(num));
+
+
+    }
+
+}
