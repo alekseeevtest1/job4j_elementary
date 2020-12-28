0e9ae4b97f797800432fe6d823fb9196139fcb8f
alekseeevtest1 <alekseeevtest@yandex.ru>
2020-12-28 22:35:38 +0300

    4.3.5. Множественное логическое выражение И

---
+++ src/ru/job4j/condition/Triangle.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Triangle {
+
+    public static boolean exist(double ab, double ac, double bc) {
+        if(ab + ac > bc && ab + bc > ac && bc +ac > ab){
+            return true;
+        }
+        return false;
+    }
+}
