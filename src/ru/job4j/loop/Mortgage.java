ddee2a546ab1162a46294784342ca6ed344f25d7
alekseeevtest1 <alekseeevtest@yandex.ru>
2020-12-31 13:47:55 +0300

    5.8. Именование классов и переменных

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -17,14 +17,13 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(int amount, int salary, double percent) {
         int year = 0;
-        while (amount > 0){
-            amount = (int) (amount + (amount * (percent/100)) - salary);
+        while (amount > 0) {
+            amount = (int) (amount + (amount * (percent / 100)) - salary);
             year++;
         }
         return year;
     }
-
     public static void main(String[] args) {
-        System.out.println(year(100,70, 50 ));
+        System.out.println(year(100, 70, 50) );
     }
 }
