90699536425f8616fd61e39e788f507bde6a6bce
alekseeevtest1 <alekseeevtest@yandex.ru>
2021-01-05 13:52:22 +0300

    6.0.2. Заполнение массива.

--- src/ru/job4j/arrays/ArrayDefenition.java
+++ src/ru/job4j/arrays/ArrayDefenition.java
@@ -8,5 +8,17 @@ public class ArrayDefenition {
         System.out.println(ages.length);
         System.out.println(surr.length);
         System.out.println(prices.length);
+
+        String[] str = new String[4];
+        str[0] = "Aleks";
+        str[1] = "Aleks1";
+        str[2] = "Aleks3";
+        str[3] = "Aleks4";
+        System.out.println();
+        System.out.println(str[0]);
+        System.out.println(str[1]);
+        System.out.println(str[2]);
+        System.out.println(str[3]);
+
     }
 }
