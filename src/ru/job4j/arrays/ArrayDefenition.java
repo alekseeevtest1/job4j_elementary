441445cf67e76deb295e043eb300f9dd575653d0
alekseeevtest1 <alekseeevtest@yandex.ru>
2021-01-05 12:42:01 +0300

    6.0.1. Объявление массива.

---
+++ src/ru/job4j/arrays/ArrayDefenition.java
@@ -0,0 +1,9 @@
+package ru.job4j.arrays;
+
+public class ArrayDefenition {
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surr = new String[100500];
+        float[] prices = new float[40];
+    }
+}
